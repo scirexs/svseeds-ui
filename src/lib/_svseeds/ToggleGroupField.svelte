4253202137da6ddc7b60703538d73d1604dfde3d
scirexs <git@scirexs.dev>
2025-05-05 15:51:03 +0000

    Replace TogglesField to ToggleGroup and ToggleGroupField.

---
+++ src/lib/_svseeds/ToggleGroupField.svelte
@@ -0,0 +1,134 @@
+<script module lang="ts">
+  export type ToggleGroupFieldProps = {
+    options: SvelteMap<string, string> | Map<string, string>,
+    label?: string,
+    extra?: string,
+    aux?: Snippet<[string, string[]]>, // Snippet<[status,values]>
+    left?: Snippet<[string, string[]]>, // Snippet<[status,values]>
+    right?: Snippet<[string, string[]]>, // Snippet<[status,values]>
+    bottom?: string,
+    descFirst?: boolean, // <false>
+    values?: string[], // bindable
+    multiple?: boolean, // <true>
+    validations?: ToggleGroupFieldValidation[],
+    status?: string, // bindable <STATE.DEFAULT>
+    style?: SVSStyle,
+    deps?: ToggleGroupFieldDeps,
+    [key: string]: unknown | Snippet,
+  };
+  export type ToggleGroupFieldDeps = {
+    svsToggleGroup?: Omit<ToggleGroupProps, ToggleGroupReqdProps | ToggleGroupBindProps | "ariaDescId" | "multiple">,
+  };
+  export type ToggleGroupFieldReqdProps = "options";
+  export type ToggleGroupFieldBindProps = "values" | "status";
+  export type ToggleGroupFieldValidation = (values: string[]) => string;
+
+  const preset = "svs-toggle-group-field";
+
+  import { type Snippet, untrack } from "svelte";
+  import { type Action } from "svelte/action";
+  import { type SvelteMap } from "svelte/reactivity";
+  import { type SVSStyle, STATE, AREA, elemId, fnClass, isNeutral } from "./core";
+  import ToggleGroup, { type ToggleGroupProps, type ToggleGroupReqdProps, type ToggleGroupBindProps } from "./_ToggleGroup.svelte";
+</script>
+
+<script lang="ts">
+  let { options, label, extra, aux, left, right, bottom, descFirst = false, values = $bindable([]), multiple = true, validations = [], status = $bindable(""), style, attributes, elements = $bindable([]), deps, ...rest }: ToggleGroupFieldProps = $props();
+
+  // *** Initialize *** //
+  if (!status) status = STATE.DEFAULT;
+  const cls = fnClass(preset, style);
+  const idLabel = elemId.get(label?.trim());
+  const idDesc = elemId.get(bottom?.trim());
+  const idErr = idDesc ?? elemId.id;
+  let message = $state(bottom);
+  let element: HTMLInputElement | undefined = $state();
+
+  // *** Initialize Deps *** //
+  const svsToggleGroup = {
+    ...Object.fromEntries(Object.entries(rest).filter(([_, v]) => typeof v === "function")),
+    ariaDescId: idDesc,
+    style: deps?.svsToggleGroup?.style as SVSStyle ?? `${preset} svs-toggle-group`,
+    action: deps?.svsToggleGroup?.action as Action,
+  };
+
+  // *** Status *** //
+  let neutral = $state(isNeutral(status) ? status : STATE.DEFAULT);
+  $effect(() => { neutral = isNeutral(status) ? status : neutral });
+  let live = $derived(status === STATE.INACTIVE ? "alert" : "status");
+  let idMsg = $derived(status === STATE.INACTIVE ? idErr : undefined);
+  function shift(oninvalid?: boolean) {
+    const vmsg = element?.validationMessage ?? "";
+    status = oninvalid && vmsg ? STATE.INACTIVE : (!values.length || vmsg) ? neutral : STATE.ACTIVE;
+    message = status === STATE.INACTIVE ? vmsg ? vmsg : bottom : bottom;
+  }
+  function verify() {
+    if (!element) return;
+    for (const v of validations) {
+      const msg = v(values);
+      if (msg) return element.setCustomValidity(msg);
+    }
+    element.setCustomValidity("");
+  }
+
+  // *** Bind Handlers *** //
+  $effect.pre(() => {
+    values;
+    untrack(() => validate());
+  });
+  function validate() {
+    verify();
+    shift();
+  }
+
+  // *** Event Handlers *** //
+  function oninvalid(ev: Event) {
+    ev.preventDefault();
+    shift(true)
+  }
+  $effect(() => untrack(() => verify()));
+</script>
+
+<!---------------------------------------->
+
+{#if options.size}
+  <div class={cls(AREA.WHOLE, status)} role="group" aria-labelledby={idLabel}>
+    {#if aux}
+      <div class={cls(AREA.TOP, status)}>
+        {@render lbl()}
+        <span class={cls(AREA.AUX, status)}>{@render aux(status, values)}</span>
+      </div>
+    {:else}
+      {@render lbl()}
+    {/if}
+    {@render desc(descFirst)}
+    <div class={cls(AREA.MIDDLE, status)}>
+      {@render side(AREA.LEFT, left)}
+      <input bind:this={element} style="display: none;" aria-hidden="true" {oninvalid} />
+      <ToggleGroup bind:values bind:ariaErrMsgId={idMsg} bind:status={neutral} {options} {multiple} {...svsToggleGroup} />
+      {@render side(AREA.RIGHT, right)}
+    </div>
+    {@render desc(!descFirst)}
+  </div>
+{/if}
+
+{#snippet lbl()}
+  {#if label?.trim()}
+    <span class={cls(AREA.LABEL, status)} id={idLabel}>
+      {label}
+      {#if extra?.trim()}
+        <span class={cls(AREA.EXTRA, status)}>{extra}</span>
+      {/if}
+    </span>
+  {/if}
+{/snippet}
+{#snippet side(area: string, body?: Snippet<[string, string[]]>)}
+  {#if body}
+    <span class={cls(area, status)}>{@render body(status, values)}</span>
+  {/if}
+{/snippet}
+{#snippet desc(show: boolean)}
+  {#if show && message?.trim()}
+    <div class={cls(AREA.BOTTOM, status)} id={idDesc ?? idErr} role={live}>{message}</div>
+  {/if}
+{/snippet}
---
+++ src/lib/_svseeds/_ToggleGroup.svelte
@@ -0,0 +1,78 @@
+<script module lang="ts">
+  export type ToggleGroupProps = {
+    options: SvelteMap<string, string> | Map<string, string>,
+    values?: string[], // bindable
+    multiple?: boolean, // <true>
+    ariaDescId?: string,
+    ariaErrMsgId?: string, // bindable
+    status?: string, // bindable <STATE.DEFAULT>
+    style?: SVSStyle,
+    action?: Action,
+    [key: string]: unknown | Snippet<[string]>,
+  };
+  export type ToggleGroupReqdProps = "options";
+  export type ToggleGroupBindProps = "values" | "ariaErrMsgId" | "status";
+
+  const preset = "svs-toggle-group";
+
+  function getSnippet(text: string, rest: Record<string, unknown>): Snippet<[string]> | undefined {
+    if (!Object.hasOwn(rest, text)) return;
+    if (typeof rest[text] !== "function") return;
+    return rest[text] as Snippet<[string]>;
+  }
+
+  import { type Snippet } from "svelte";
+  import { type Action } from "svelte/action";
+  import { type SvelteMap } from "svelte/reactivity";
+  import { type SVSStyle, STATE, AREA, fnClass } from "./core";
+</script>
+
+<script lang="ts">
+  let { options, values = $bindable([]), multiple = true, ariaDescId, ariaErrMsgId = $bindable(), status = $bindable(""), style, attributes, action, ...rest }: ToggleGroupProps = $props();
+
+  // *** Initialize *** //
+  if (!status) status = STATE.DEFAULT;
+  const cls = fnClass(preset, style);
+  const role = multiple ? "checkbox" : "radio";
+  const roleGroup = multiple ? "group" : "radiogroup";
+
+  // *** Bind Handlers *** //
+  let opts = $derived([...options.entries().map(([value, text]) => ({ value, text, checked: values.includes(value) }))]);
+  let invalid = $derived(ariaErrMsgId ? true : undefined);
+
+  // *** Event Handlers *** //
+  const update = multiple
+    ? (value: string) => (values.includes(value) ? values.filter((x) => x !== value) : opts.map((x) => x.value).filter((x) => [...values, value].includes(x)))
+    : (value: string) => (values.includes(value) ? [] : [value]);
+  function updateValues(value: string): () => void {
+    return () => values = update(value);
+  }
+</script>
+
+<!---------------------------------------->
+
+{#if opts.length}
+  <span class={cls(AREA.WHOLE, status)} role={roleGroup} aria-describedby={ariaDescId} aria-invalid={!multiple ? invalid : undefined} aria-errormessage={!multiple ? ariaErrMsgId : undefined}>
+    {#each opts as { value, text, checked } (value)}
+      {@const c = cls(AREA.MAIN, checked ? STATE.ACTIVE : status)}
+      {#if action}
+        <button class={c} aria-checked={checked} aria-invalid={multiple ? invalid : undefined} aria-errormessage={multiple ? ariaErrMsgId : undefined} onclick={updateValues(value)} type="button" {role} use:action>
+          {@render content(value, text)}
+        </button>
+      {:else}
+        <button class={c} aria-checked={checked} aria-invalid={multiple ? invalid : undefined} aria-errormessage={multiple ? ariaErrMsgId : undefined} onclick={updateValues(value)} type="button" {role}>
+          {@render content(value, text)}
+        </button>
+      {/if}
+    {/each}
+  </span>
+{/if}
+
+{#snippet content(value: string, text: string)}
+  {@const snippet = getSnippet(text, rest)}
+  {#if snippet}
+    {@render snippet(value)}
+  {:else}
+    {text}
+  {/if}
+{/snippet}
