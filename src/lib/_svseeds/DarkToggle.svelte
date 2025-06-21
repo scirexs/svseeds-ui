86fcd3011bef74c43127df31b384352ea3e79752
scirexs <git@scirexs.dev>
2025-06-21 15:55:55 +0000

    fix: add children prop to DarkToggle

--- src/lib/_svseeds/DarkToggle.svelte
+++ src/lib/_svseeds/DarkToggle.svelte
@@ -4,6 +4,7 @@
   default value: *`(value)`*
   ```ts
   interface DarkToggleProps {
+    children?: Snippet<[boolean, string, HTMLButtonElement | undefined]>; // Snippet<[value,variant,element]>
     dark?: boolean; // bindable (prefers-color-scheme)
     element?: HTMLButtonElement; // bindable
     variant?: string; // bindable (VARIANT.NEUTRAL)
@@ -16,8 +17,8 @@
   ### Anatomy
   ```svelte
   <Toggle {...deps.svsToggle} bind:value={dark} bind:element>
-    {#if deps.svsToggle.children}
-      {deps.svsToggle.children}
+    {#if children}
+      {children}
     {:else}
       // Default Icon
     {/if}
@@ -26,6 +27,7 @@
 -->
 <script module lang="ts">
   export interface DarkToggleProps {
+    children?: Snippet<[boolean, string, HTMLButtonElement | undefined]>; // Snippet<[value,variant,element]>
     dark?: boolean; // bindable (prefers-color-scheme)
     element?: HTMLButtonElement; // bindable
     variant?: string; // bindable (VARIANT.NEUTRAL)
@@ -182,12 +184,13 @@
   const twColors = new Set<string>();
   const theme = new Theme();
 
+  import { type Snippet } from "svelte";
   import { VARIANT, omit } from "./core";
   import Toggle, { type ToggleProps, type ToggleReqdProps, type ToggleBindProps } from "./_Toggle.svelte";
 </script>
 
 <script lang="ts">
-  let { dark = $bindable(), variant = $bindable(""), element = $bindable(), deps }: DarkToggleProps = $props();
+  let { children, dark = $bindable(), variant = $bindable(""), element = $bindable(), deps }: DarkToggleProps = $props();
 
   // *** Initialize *** //
   if (!variant) variant = VARIANT.NEUTRAL;
@@ -196,7 +199,8 @@
 
   // *** Initialize Deps *** //
   const svsToggle = {
-    ...omit(deps?.svsToggle, "children", "styling", "attributes"),
+    ...omit(deps?.svsToggle, "styling", "attributes"),
+    children: main,
     styling: deps?.svsToggle?.styling ?? `${preset} svs-toggle`,
     ariaLabel,
     attributes: {
@@ -210,20 +214,19 @@
 
 <!---------------------------------------->
 
-<Toggle bind:value={dark} bind:variant bind:element {...svsToggle}>
-  {#snippet children(value: boolean, variant: string, element: HTMLButtonElement | undefined)}
-    {#if deps?.svsToggle?.children}
-      {@render deps.svsToggle.children(value, variant, element)}
-    {:else}
-      {#if value}
-        {@render svgDark()}
-      {:else}
-        {@render svgLight()}
-      {/if}
-    {/if}
-  {/snippet}
-</Toggle>
+<Toggle bind:value={dark} bind:variant bind:element {...svsToggle} />
 
+{#snippet main(value: boolean, variant: string, element: HTMLButtonElement | undefined)}
+  {#if children}
+    {@render children(value, variant, element)}
+  {:else}
+    {#if value}
+      {@render svgDark()}
+    {:else}
+      {@render svgLight()}
+    {/if}
+  {/if}
+{/snippet}
 {#snippet svgDark()}
   <svg style="width:100%;height:100%;" viewBox="0 0 24 24" aria-hidden="true">
     <path fill-rule="evenodd" clip-rule="evenodd" d="M12.2256 2.00253C9.59172 1.94346 6.93894 2.9189 4.92893 4.92891C1.02369 8.83415 1.02369 15.1658 4.92893 19.071C8.83418 22.9763 15.1658 22.9763 19.0711 19.071C21.0811 17.061 22.0565 14.4082 21.9975 11.7743C21.9796 10.9772 21.8669 10.1818 21.6595 9.40643C21.0933 9.9488 20.5078 10.4276 19.9163 10.8425C18.5649 11.7906 17.1826 12.4053 15.9301 12.6837C14.0241 13.1072 12.7156 12.7156 12 12C11.2844 11.2844 10.8928 9.97588 11.3163 8.0699C11.5947 6.81738 12.2094 5.43511 13.1575 4.08368C13.5724 3.49221 14.0512 2.90664 14.5935 2.34046C13.8182 2.13305 13.0228 2.02041 12.2256 2.00253ZM17.6569 17.6568C18.9081 16.4056 19.6582 14.8431 19.9072 13.2186C16.3611 15.2643 12.638 15.4664 10.5858 13.4142C8.53361 11.362 8.73568 7.63895 10.7814 4.09281C9.1569 4.34184 7.59434 5.09193 6.34315 6.34313C3.21895 9.46732 3.21895 14.5326 6.34315 17.6568C9.46734 20.781 14.5327 20.781 17.6569 17.6568Z" />
